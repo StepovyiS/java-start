952c1dca99864eff2f8d50177af412fe2a38fc6f
Sasha <stepowoi.s@gmail.com>
2018-08-17 13:16:59 +0300

    unused

---
+++ src/MyClassTask2_1.java
@@ -0,0 +1,14 @@
+public class MyClassTask2_1 {
+    public static void main(String[]args){
+        String s1 = "new";
+        String s2 = "task";
+        String s3 = "485876";
+        String s4 = s1+s3;
+        String s5 = s3+s2+s1;
+        String s6 = s1+s2+s3;
+        System.out.println(s4);
+        System.out.println(s5);
+        System.out.println(s6);
+    }
+
+}
