efb91d029ad48e3b600c645564fb120f261c9cb4
Sasha <stepowoi.s@gmail.com>
2018-08-17 17:22:48 +0300

    unused

---
+++ src/MyClassTask2_2.java
@@ -0,0 +1,15 @@
+public class MyClassTask2_2 {
+    public static void main(String[]args){
+        short x = 12;
+        short y = 15;
+        int sum = x+y;
+        int sub = y -x;
+        int mul = y*y;
+        double div = y/x;
+        System.out.println(sum);
+        System.out.println(sub);
+        System.out.println(mul);
+        System.out.println(div);
+
+    }
+}
