adaa382ad43c59cbab3284248d3888399abc89bb
Sasha <stepowoi.s@gmail.com>
2018-08-22 17:41:02 +0300

    unused

---
+++ src/MyClassTask4_4.java
@@ -0,0 +1,11 @@
+public class MyClassTask4_4 {
+    public static void main(String[] args) {
+        int[] mass;
+        mass = new int[]{12, 44, 2, 4};
+        int sum = 0;
+        for (int i = 1; i < mass.length - 1; i++)
+            sum += mass[i];
+        System.out.println("Сумма" + sum);
+    }
+}
+
