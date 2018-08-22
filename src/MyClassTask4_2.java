ef8443a173fc99a063f1408e7167f8e51db81e34
Sasha <stepowoi.s@gmail.com>
2018-08-22 12:21:56 +0300

    unused

---
+++ src/MyClassTask4_2.java
@@ -0,0 +1,12 @@
+public class MyClassTask4_2 {
+    public static void main(String[]args){
+        int[] mass;
+        mass = new int[100];
+        for (int i = 3; i<=100; i+=3) {
+            mass[i] = i;
+            System.out.println(i);
+        }
+
+        }
+    }
+
