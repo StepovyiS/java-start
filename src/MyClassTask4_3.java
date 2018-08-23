9018bd6932b8d57104bf45d68087c7cd9fc92f55
Sasha <stepowoi.s@gmail.com>
2018-08-23 11:02:33 +0300

    unused

--- src/MyClassTask4_3.java
+++ src/MyClassTask4_3.java
@@ -3,8 +3,9 @@ public class MyClassTask4_3 {
         int[] mass;
         mass=new int[101];
         for (int i = 100;  i>0 ; i--){
-            mass[i] = i;
-            System.out.println(i);
+            int sum=0;
+            sum +=mass[i];
+
         }
 
 
