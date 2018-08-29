1a3b4bc5c38d11fe75fd4f928fef777d48b2d29b
Sasha <stepowoi.s@gmail.com>
2018-08-29 17:34:31 +0300

    unused

--- src/MyClassTask3_3.java
+++ src/MyClassTask3_3.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 public class MyClassTask3_3 {
     public static void main(String[]args){
         Scanner d = new Scanner(System.in);
-        int[]books;
-        books=new int[4];
-        books[0]=d.nextInt();
-        books[1]=d.nextInt();
-        books[2]=d.nextInt();
-        books[3]=d.nextInt();
-        int[]books1,books2 = new int [4];
-        books1= Arrays.copyOfRange(books, 0, 4);
-        books2 = Arrays.copyOfRange(books, 0, 4);
-        String books1a= Arrays.toString(books1);
-        String books2b = Arrays.toString(books2);
-        System.out.println(books1a);
-        System.out.println(books2b);
+        int[][]books;
+        books=new int[4][4];
+        for (int a=0;a<books.length; a++)
+
+            System.out.println(a);
+        for(int b =0;b<books.length;b++)
+        System.out.println(b);
+
+
+
+
 
     }
 
