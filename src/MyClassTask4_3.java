2115c08611f93c40fa6a632efecdaed3d54bbcd9
Sasha <stepowoi.s@gmail.com>
2018-08-29 17:15:52 +0300

    unused

--- src/MyClassTask4_3.java
+++ src/MyClassTask4_3.java
@@ -2,8 +2,9 @@ public class MyClassTask4_3 {
     public static void main(String[] args) {
         int[] mass;
         mass=new int[101];
-        for (int i = 100;  i>0 ; i--){
-            mass[i]= 100-1;
+        for (int i = 100;  i>=0 ; i--){
+            mass[i]= 100-i-1;
+
 
 
 
