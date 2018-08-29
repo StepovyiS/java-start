86717508bc84fc670eccc6b937e83dab2509504b
Sasha <stepowoi.s@gmail.com>
2018-08-29 19:02:09 +0300

    unused

--- src/MyClassTask3_4.java
+++ src/MyClassTask3_4.java
@@ -1,18 +1,14 @@
 import java.util.Arrays;
 
 public class MyClassTask3_4 {
-
-
     public static void main(String[]args){
-        String[] a ;
-        a = new String[5];
-        a[0]=String.valueOf("23");
-        a[1]= String.valueOf("34");
-        a[2]= String.valueOf("32");
-        a[3]= String.valueOf("11");
-        a[4]= String.valueOf("13");
-        Arrays.sort(a);
-        System.out.println(Arrays.toString(a));
+        int[]b;
+       b=new int[args.length];
+       for( int a=0; a<args.length; a++)
+           b[a]=Integer.valueOf(args[a]);
+      Arrays.sort(b);
+
+        System.out.println(Arrays.toString(b));
 
 
 
